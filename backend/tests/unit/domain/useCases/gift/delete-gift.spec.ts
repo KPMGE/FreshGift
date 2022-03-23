7f41e35dba83588fc14eb0161135ccd9eb3e15ea
KPMGE <jesuskevin254@gmail.com>
2022-03-23 19:19:09 -0300

    :white_check_mark: ensure delete-gift returns deleted gift

--- backend/tests/unit/domain/useCases/gift/delete-gift.spec.ts
+++ backend/tests/unit/domain/useCases/gift/delete-gift.spec.ts
@@ -1,32 +1,43 @@
+import { Gift } from "../../../../../src/domain/entities";
+
 interface DeleteGift {
-  execute(giftId: string): Promise<void>;
+  execute(giftId: string): Promise<Gift>;
 }
 
 interface DeleteGiftRepository {
-  delete(giftId: string): Promise<void>;
+  delete(giftId: string): Promise<Gift>;
 }
 
 class DeleteGiftService implements DeleteGift {
   constructor(private readonly deleteRepository: DeleteGiftRepository) {}
 
-  async execute(id: string): Promise<void> {
-    this.deleteRepository.delete(id);
+  async execute(id: string): Promise<Gift> {
+    return this.deleteRepository.delete(id);
   }
 }
 
 class DeleteGiftRepositoryMock implements DeleteGiftRepository {
   giftId: string;
-  async delete(giftId: string): Promise<void> {
+  output: Gift = {
+    id: "any_gift_id",
+    name: "any_gift_name",
+    description: "any_gift_description",
+    price: 0,
+    imageUrl: "any_gift_image",
+  };
+
+  async delete(giftId: string): Promise<Gift> {
     this.giftId = giftId;
+    return this.output;
   }
 }
 
 type SutTypes = {
   sut: DeleteGift;
-  repositoryMock: DeleteGiftRepository;
+  deleteRepository: DeleteGiftRepositoryMock;
 };
 
-const makeSut = () => {
+const makeSut = (): SutTypes => {
   const deleteRepository = new DeleteGiftRepositoryMock();
   const sut = new DeleteGiftService(deleteRepository);
 
@@ -37,13 +48,27 @@ const makeSut = () => {
 };
 
 describe("delete-gift", () => {
-  const giftId = "any_gift_id";
+  const fakeGift = {
+    id: "any_gift_id",
+    name: "any_gift_name",
+    description: "any_gift_description",
+    price: 0,
+    imageUrl: "any_gift_image",
+  };
 
-  it("should call delete repository with correct data.", () => {
+  it("should call delete repository with correct data.", async () => {
     const { sut, deleteRepository } = makeSut();
 
-    sut.execute(giftId);
+    sut.execute(fakeGift.id);
 
-    expect(deleteRepository.giftId).toBe(giftId);
+    expect(deleteRepository.giftId).toBe(fakeGift.id);
+  });
+
+  it("should return the deleted gift", async () => {
+    const { sut } = makeSut();
+
+    const deletedGift = await sut.execute(fakeGift.id);
+
+    expect(deletedGift).toEqual(fakeGift);
   });
 });
