a4c62c88a30a69449317d79bd9ac66be7ba02f71
KPMGE <jesuskevin254@gmail.com>
2022-03-26 16:16:48 -0300

    :white_check_mark: ensure get-user returns a valid user

--- backend/tests/unit/domain/useCases/user/get-user.spec.ts
+++ backend/tests/unit/domain/useCases/user/get-user.spec.ts
@@ -1,50 +1,79 @@
+import { User } from "../../../../../src/domain/entities"
+
 interface GetUser {
-  execute(userId: string): Promise<void>
+  execute(userId: string): Promise<User>
 }
 
 interface GetUserRepository {
-  get(userId: string): Promise<void>
+  get(userId: string): Promise<User>
 }
 
-class GetUserRepositoryMock implements GetUserRepository {
+class GetUserRepositorySpy implements GetUserRepository {
   userId: string = 'any_user_id'
+  output: User = {
+    id: 'any_user_id',
+    name: 'any_name',
+    email: 'any_valid@gmail.com',
+    password: 'any_password',
+    confirmPassword: 'any_password',
+    userName: 'any_username',
+    savedGifts: []
+  }
 
-  async get(userId: string): Promise<void> {
+  async get(userId: string): Promise<User> {
     this.userId = userId
+    return this.output
   }
 }
 
 class GetUserService implements GetUser {
-  constructor(private readonly getUserRepository: GetUserRepositoryMock) { }
+  constructor(private readonly getUserRepository: GetUserRepositorySpy) { }
 
-  async execute(userId: string): Promise<void> {
-    this.getUserRepository.get(userId)
+  async execute(userId: string): Promise<User> {
+    return this.getUserRepository.get(userId)
   }
 }
 
 type SutTypes = {
   sut: GetUserService,
-  getUserRepositoryMock: GetUserRepositoryMock
+  getUserRepositorySpy: GetUserRepositorySpy
 }
 
 const makeSut = (): SutTypes => {
-  const getUserRepositoryMock = new GetUserRepositoryMock()
-  const sut = new GetUserService(getUserRepositoryMock)
+  const getUserRepositorySpy = new GetUserRepositorySpy()
+  const sut = new GetUserService(getUserRepositorySpy)
 
   return {
     sut,
-    getUserRepositoryMock
+    getUserRepositorySpy
   }
 }
 
 describe('get-user', () => {
-  const userId = 'any_user_id'
+  const fakeUser: User = {
+    id: 'any_user_id',
+    name: 'any_name',
+    email: 'any_valid@gmail.com',
+    password: 'any_password',
+    confirmPassword: 'any_password',
+    userName: 'any_username',
+    savedGifts: []
+  }
 
   it('should call repository with right id', async () => {
-    const { sut, getUserRepositoryMock } = makeSut()
+    const { sut, getUserRepositorySpy } = makeSut()
 
-    await sut.execute(userId)
+    await sut.execute(fakeUser.id)
 
-    expect(getUserRepositoryMock.userId).toBe(userId)
+    expect(getUserRepositorySpy.userId).toBe(fakeUser.id)
+  })
+
+
+  it('should return a valid user', async () => {
+    const { sut, getUserRepositorySpy } = makeSut()
+
+    const user = await sut.execute(fakeUser.id)
+
+    expect(getUserRepositorySpy.output).toBe(user)
   })
 })
