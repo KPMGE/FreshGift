c7071d0605728fefad09c2b8b25419cb34af1bc5
KPMGE <jesuskevin254@gmail.com>
2022-03-26 22:40:30 -0300

    :white_check_mark: ensure calls create-gift controller with valid data

---
+++ backend/tests/unit/presentation/create-gift-controller.spec.ts
@@ -0,0 +1,76 @@
+import { CreateGiftService } from "../../../src/data/services/gift"
+import { Gift } from "../../../src/domain/entities"
+import { CreateGift } from "../../../src/domain/useCases/gift"
+import { SaveGiftRepositoryMock } from "../domain/repositories/gift"
+
+interface HttpResponse<T = any> {
+  statusCode: number
+  data: T
+}
+
+interface HttpRequest<T = any> {
+  body?: T
+}
+
+interface Controller {
+  handle(req?: HttpRequest): Promise<HttpResponse>
+}
+
+class CreateGiftController implements Controller {
+  constructor(private readonly createGiftService: CreateGift) { }
+
+  async handle(req?: HttpRequest<any>): Promise<HttpResponse<Gift>> {
+    if (!req) throw new Error('no request!')
+    if (!req.body) new Error('missing body')
+
+    return this.createGiftService.execute(req?.body)
+  }
+}
+
+type SutTypes = {
+  sut: CreateGiftController,
+  createGiftService: CreateGiftService
+  saveGiftRepository: SaveGiftRepositoryMock
+}
+
+const makeSut = (): SutTypes => {
+  const saveGiftRepository = new SaveGiftRepositoryMock()
+  const createGiftService = new CreateGiftService(saveGiftRepository)
+  const sut = new CreateGiftController(createGiftService)
+
+  return {
+    sut,
+    saveGiftRepository,
+    createGiftService,
+  }
+}
+
+describe('create-gift-controller', () => {
+  const fakeGift: Gift = {
+    id: 'some_gift_id',
+    name: 'some_gift_name',
+    price: 100,
+    imageUrl: 'some_image_url',
+    description: 'some_description'
+  }
+
+  const fakeRequest: HttpRequest<Gift> = {
+    body: fakeGift
+  }
+
+  it('should throw an error if no request is provided', async () => {
+    const { sut } = makeSut()
+
+    const promise = sut.handle()
+
+    await expect(promise).rejects.toThrowError()
+  })
+
+  it('should throw an error if no request body is provided', async () => {
+    const { sut } = makeSut()
+
+    const promise = sut.handle()
+
+    await expect(promise).rejects.toThrowError()
+  })
+})
