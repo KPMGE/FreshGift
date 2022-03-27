247602a18a3386ac3705f1f321f74ca9b0d579e0
KPMGE <jesuskevin254@gmail.com>
2022-03-27 16:58:09 -0300

    :white_check_mark: ensure deleGiftController deletes the gift

--- backend/src/infra/repositories/fake/gift-repository.ts
+++ backend/src/infra/repositories/fake/gift-repository.ts
@@ -1,12 +1,26 @@
 import { SaveGiftRepository } from '../../../data/contracts/gift'
+import { DeleteUserRepository } from '../../../data/contracts/user'
 import { Gift } from '../../../domain/entities'
 
+let listGifts: Gift[] = []
+
 export class FakeSaveGiftRepository implements SaveGiftRepository {
-  listGifts: Gift[] = []
 
   async save(gift: Gift): Promise<Gift> {
-    this.listGifts.push(gift)
+    listGifts.push(gift)
     return gift
   }
 }
 
+export class FakeDeleteGiftRepository implements DeleteUserRepository {
+  async delete(giftId: string): Promise<Gift> {
+    const foundGift = listGifts.find(gift => gift.id === giftId)
+
+    const newList = listGifts.filter(gift => gift.id !== giftId)
+    listGifts = newList
+
+    if (!foundGift) throw new Error('gift not found')
+
+    return foundGift
+  }
+}
--- backend/tests/integration/presentation/gift/delete-gift.spec.ts
+++ backend/tests/integration/presentation/gift/delete-gift.spec.ts
@@ -1,10 +1,11 @@
 import { DeleteGiftService } from "../../../../src/data/services/gift"
+import { Gift } from "../../../../src/domain/entities"
 import { MissingParameterError } from "../../../../src/domain/errors"
 import { DeleteGift } from "../../../../src/domain/useCases/gift"
+import { FakeDeleteGiftRepository, FakeSaveGiftRepository } from "../../../../src/infra/repositories"
 import { HttpRequest, HttpResponse, ok, serverError } from "../../../../src/presentation/contracts"
 import { Controller } from "../../../../src/presentation/contracts/controller"
 import { GiftViewModel } from "../../../../src/presentation/view-models"
-import { DeleteGiftRepositorySpy } from "../../domain/repositories/gift"
 
 class DeleteGiftController implements Controller {
   constructor(private readonly deleteGiftService: DeleteGift) { }
@@ -19,16 +20,35 @@ class DeleteGiftController implements Controller {
   }
 }
 
+type SutTypes = {
+  sut: DeleteGiftController
+}
+
+const makeSut = (): SutTypes => {
+  const deleteGiftRepository = new FakeDeleteGiftRepository()
+  const deleteGiftService = new DeleteGiftService(deleteGiftRepository)
+  const sut = new DeleteGiftController(deleteGiftService)
+
+  return {
+    sut
+  }
+}
+
 describe('delete-gift', () => {
+  const fakeGift: Gift = {
+    id: 'any_gift_id',
+    name: 'some_name',
+    price: 100,
+    imageUrl: 'some_image_url',
+    description: 'some_description'
+  }
+
   const fakeRequest: HttpRequest<{ giftId: string }> = {
     body: { giftId: 'any_gift_id' }
   }
 
   it('should return serverError if service throws', async () => {
-    const deleteGiftRepository = new DeleteGiftRepositorySpy()
-    const deleteGiftService = new DeleteGiftService(deleteGiftRepository)
-    const sut = new DeleteGiftController(deleteGiftService)
-
+    const { sut } = makeSut()
     const response = await sut.handle({})
 
     expect(response).toEqual(serverError(new MissingParameterError('id')))
@@ -36,12 +56,18 @@ describe('delete-gift', () => {
   })
 
   it('should return a valid HttpResponse with a valid deletedGift', async () => {
-    const deleteGiftRepository = new DeleteGiftRepositorySpy()
-    const deleteGiftService = new DeleteGiftService(deleteGiftRepository)
-    const sut = new DeleteGiftController(deleteGiftService)
+    const saveGiftRepository = new FakeSaveGiftRepository()
+    await saveGiftRepository.save(fakeGift)
 
-    const response = await sut.handle(fakeRequest)
+    const { sut } = makeSut()
+
+    let response = await sut.handle(fakeRequest)
 
     expect(response.statusCode).toBe(200)
+    expect(response.data).toEqual(fakeGift)
+
+    response = await sut.handle(fakeRequest)
+
+    expect(response.statusCode).toBe(500)
   })
 })
