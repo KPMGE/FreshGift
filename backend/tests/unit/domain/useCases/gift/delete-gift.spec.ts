74b1bbf5bac88d6b7e2daf64ec367753f49a0b24
KPMGE <jesuskevin254@gmail.com>
2022-03-28 07:40:54 -0300

    :white_check_mark: ensure DeleteGiftService retuns deleted gift

--- backend/tests/unit/domain/useCases/gift/delete-gift.spec.ts
+++ backend/tests/unit/domain/useCases/gift/delete-gift.spec.ts
@@ -1,22 +1,22 @@
-import { DeleteGift } from "../../../../../src/domain/useCases/gift";
-import { DeleteGiftService } from "../../../../../src/data/services/gift";
-import { DeleteGiftRepositorySpy } from "../../repositories/gift";
-import { MissingParameterError } from "../../../../../src/domain/errors";
+import { DeleteGift } from "../../../../../src/domain/useCases/gift"
+import { DeleteGiftService } from "../../../../../src/data/services/gift"
+import { DeleteGiftRepositorySpy } from "../../repositories/gift"
+import { MissingParameterError } from "../../../../../src/domain/errors"
 
 type SutTypes = {
-  sut: DeleteGift;
-  deleteRepository: DeleteGiftRepositorySpy;
-};
+  sut: DeleteGift
+  deleteRepository: DeleteGiftRepositorySpy
+}
 
 const makeSut = (): SutTypes => {
-  const deleteRepository = new DeleteGiftRepositorySpy();
-  const sut = new DeleteGiftService(deleteRepository);
+  const deleteRepository = new DeleteGiftRepositorySpy()
+  const sut = new DeleteGiftService(deleteRepository)
 
   return {
     sut,
     deleteRepository,
-  };
-};
+  }
+}
 
 describe("delete-gift", () => {
   const fakeGift = {
@@ -25,30 +25,38 @@ describe("delete-gift", () => {
     description: "any_gift_description",
     price: 0,
     imageUrl: "any_gift_image",
-  };
+  }
 
   it("should throw an error if no id is provided", async () => {
-    const { sut } = makeSut();
+    const { sut } = makeSut()
 
-    const promise = sut.execute(undefined);
+    const promise = sut.execute(undefined)
 
-    await expect(promise).rejects.toThrowError(new MissingParameterError('id'));
-  });
+    await expect(promise).rejects.toThrowError(new MissingParameterError('id'))
+  })
 
 
   it("should call delete repository with correct data.", async () => {
-    const { sut, deleteRepository } = makeSut();
+    const { sut, deleteRepository } = makeSut()
 
-    await sut.execute(fakeGift.id);
+    await sut.execute(fakeGift.id)
 
-    expect(deleteRepository.giftId).toBe(fakeGift.id);
-  });
+    expect(deleteRepository.giftId).toBe(fakeGift.id)
+  })
 
   it("should call repository only once", async () => {
-    const { sut, deleteRepository } = makeSut();
+    const { sut, deleteRepository } = makeSut()
 
-    await sut.execute(fakeGift.id);
+    await sut.execute(fakeGift.id)
 
-    expect(deleteRepository.callsCount).toBe(1);
-  });
-});
+    expect(deleteRepository.callsCount).toBe(1)
+  })
+
+  it("should return deleted gift", async () => {
+    const { sut } = makeSut()
+
+    const deletedGift = await sut.execute(fakeGift.id)
+
+    expect(deletedGift).toEqual(fakeGift)
+  })
+})
