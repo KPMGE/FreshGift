1c258fc4cb6c69f915e36c2725e14bcfc1adae2e
KPMGE <jesuskevin254@gmail.com>
2022-03-29 15:19:10 -0300

    :white_check_mark: ensure get-user-controller returns a valid user

--- backend/tests/integration/presentation/user/get-user.spec.ts
+++ backend/tests/integration/presentation/user/get-user.spec.ts
@@ -1,10 +1,10 @@
 import { GetUserService } from "../../../../src/data/services/user/get-user"
 import { GetUser } from "../../../../src/domain/useCases/user"
+import { FakeGetUserReposioty, FakeRegisterUserRepository } from "../../../../src/infra/repositories/fake/user-repository"
 import { HttpRequest, HttpResponse, ok } from "../../../../src/presentation/contracts"
 import { Controller } from "../../../../src/presentation/contracts/controller"
-import { GetUserRepositorySpy } from "../../../unit/domain/repositories/user/get-user"
 
-class GetUserByIdController implements Controller {
+class GetUserController implements Controller {
   constructor(private readonly getUserService: GetUser) { }
 
   async handle(req?: HttpRequest): Promise<HttpResponse<GetUserService.Result>> {
@@ -13,8 +13,34 @@ class GetUserByIdController implements Controller {
   }
 }
 
+type SutTypes = {
+  sut: GetUserController
+}
+
+const makeSut = (): SutTypes => {
+  const repo = new FakeGetUserReposioty()
+  const service = new GetUserService(repo)
+  const sut = new GetUserController(service)
+  return {
+    sut
+  }
+}
+
 describe('get-user', () => {
-  const fakeUserId = 'any_user_id'
+  beforeAll(async () => {
+    const registerUser = new FakeRegisterUserRepository()
+    await registerUser.register({
+      id: 'any_id',
+      name: 'any_name',
+      email: 'any_valid_email',
+      password: 'any_password',
+      confirmPassword: 'any_password',
+      userName: 'any_username',
+      savedGifts: []
+    })
+  })
+
+  const fakeUserId = 'any_id'
   const fakeRequest: HttpRequest = {
     params: {
       userId: fakeUserId
@@ -22,9 +48,7 @@ describe('get-user', () => {
   }
 
   it('should return a valid user', async () => {
-    const repo = new GetUserRepositorySpy()
-    const service = new GetUserService(repo)
-    const sut = new GetUserByIdController(service)
+    const { sut } = makeSut()
 
     const user = await sut.handle(fakeRequest)
 
