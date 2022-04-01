1743e46d5b6846c35feec60f9d76252457ed0880
KPMGE <jesuskevin254@gmail.com>
2022-04-01 11:05:40 -0300

    :recycle: create interfaces for useCase and repository

--- backend/tests/data/authentication.spec.ts
+++ backend/tests/data/authentication.spec.ts
@@ -1,4 +1,19 @@
-class LoadAccountByEmailRepositoryMock {
+namespace AuthenticationUseCase {
+  export type Props = {
+    password: string
+    email: string
+  }
+}
+
+interface AuthenticationUseCase {
+  execute(input: AuthenticationUseCase.Props): Promise<void>
+}
+
+interface LoadAccountByEmailRepository {
+  load(email: string): Promise<void>
+}
+
+class LoadAccountByEmailRepositoryMock implements LoadAccountByEmailRepository {
   input?: string
   async load(email: string): Promise<void> {
     this.input = email
@@ -6,8 +21,8 @@ class LoadAccountByEmailRepositoryMock {
 }
 
 class AuthenticationService {
-  constructor(private readonly loadAccountByEmailRepository: LoadAccountByEmailRepositoryMock) { }
-  async execute(email: string): Promise<void> {
+  constructor(private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository) { }
+  async execute({ email }: AuthenticationUseCase.Props): Promise<void> {
     await this.loadAccountByEmailRepository.load(email)
   }
 }
@@ -22,7 +37,7 @@ describe('authentication', () => {
     const repositoryMock = new LoadAccountByEmailRepositoryMock()
     const sut = new AuthenticationService(repositoryMock)
 
-    await sut.execute(fakeInput.email)
+    await sut.execute(fakeInput)
 
     expect(repositoryMock.input).toBe(fakeInput.email)
   })
