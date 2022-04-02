887d04914b57fed761b98ad77b70f353a3bf881e
KPMGE <jesuskevin254@gmail.com>
2022-04-02 18:09:00 -0300

    :white_check_mark: ensure CompareFieldsValidation retuns nothing if the fields are equal

--- backend/tests/validation/compare-fields.spec.ts
+++ backend/tests/validation/compare-fields.spec.ts
@@ -12,6 +12,7 @@ class CompareFieldsValidation implements Validator {
 describe('compare-fields-validation', () => {
   const fieldName = 'any_name'
   const fieldToCompareName = 'any_other_name'
+
   it('should return InvalidParamError if the validation does not succeed', () => {
     const sut = new CompareFieldsValidation(fieldName, fieldToCompareName)
     const error = sut.validate({
@@ -19,5 +20,14 @@ describe('compare-fields-validation', () => {
       [fieldToCompareName]: 'any_other_value'
     })
     expect(error).toEqual(new InvalidParamError(fieldName))
+  })
+
+  it('should return nothing if the fields are equal', () => {
+    const sut = new CompareFieldsValidation(fieldName, fieldToCompareName)
+    const error = sut.validate({
+      [fieldName]: 'any_value',
+      [fieldToCompareName]: 'any_value'
+    })
+    expect(error).toBeFalsy()
   })
 })
