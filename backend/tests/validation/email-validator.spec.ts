5bf9c68c91ff9b2ff7cd5b2d83f4c2afc704f01f
KPMGE <jesuskevin254@gmail.com>
2022-04-02 17:11:26 -0300

    :white_check_mark: ensure EmailValidation returns InvalidParamError when EmailValidator returns false

--- backend/tests/validation/email-validator.spec.ts
+++ backend/tests/validation/email-validator.spec.ts
@@ -1,11 +1,14 @@
+import { InvalidEmailError } from "../../src/domain/errors"
 import { Validator } from "../../src/presentation/contracts"
+import { InvalidParamError } from "../../src/presentation/errors"
 import { EmailValidator } from "../../src/validation/contracts"
 
 class EmailValidatorSpy implements EmailValidator {
   input?: string
+  output: boolean = true
   isValid(email: string): boolean {
     this.input = email
-    return true
+    return this.output
   }
 }
 
@@ -16,11 +19,26 @@ class EmailValidation implements Validator {
   ) { }
 
   validate(input: any): Error {
-    this.emailValidator.isValid(input[this.fieldName])
-    return null
+    const isEmailValid = this.emailValidator.isValid(input[this.fieldName])
+    if (!isEmailValid) return new InvalidParamError(fieldName)
   }
 }
 
+type SutTypes = {
+  sut: EmailValidation,
+  emailValidatorSpy: EmailValidatorSpy
+}
+
+const fieldName = 'email'
+
+const makeSut = (): SutTypes => {
+  const emailValidatorSpy = new EmailValidatorSpy()
+  const sut = new EmailValidation(fieldName, emailValidatorSpy)
+  return {
+    emailValidatorSpy,
+    sut
+  }
+}
 
 describe('email-validation', () => {
   const fakeInput = {
@@ -28,11 +46,15 @@ describe('email-validation', () => {
   }
 
   it('should call email validator with right data', () => {
-    const emailValidatorSpy = new EmailValidatorSpy()
-    const sut = new EmailValidation('email', emailValidatorSpy)
-
+    const { sut, emailValidatorSpy } = makeSut()
     sut.validate(fakeInput)
-
     expect(emailValidatorSpy.input).toBe(fakeInput.email)
+  })
+
+  it('should return error if emailValidator throws', () => {
+    const { sut, emailValidatorSpy } = makeSut()
+    emailValidatorSpy.output = false
+    const error = sut.validate(fakeInput)
+    expect(error).toEqual(new InvalidParamError(fieldName))
   })
 })
