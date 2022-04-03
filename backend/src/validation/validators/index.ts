742e7d27b60f9c50a8a075d8727e0e6775656352
KPMGE <jesuskevin254@gmail.com>
2022-04-02 23:17:21 -0300

    :recycle: move ValidationComposite to its right folder

--- backend/src/validation/validators/index.ts
+++ backend/src/validation/validators/index.ts
@@ -1,3 +1,4 @@
 export * from './email-validation'
 export * from './required-parameter'
 export * from './compare-fields'
+export * from './validation-composite'
--- backend/tests/validation/validation-composite.spec.ts
+++ backend/tests/validation/validation-composite.spec.ts
@@ -1,16 +1,6 @@
-import { Validator } from "../../src/presentation/contracts"
 import { MissingParamError } from "../../src/presentation/errors"
+import { ValidationComposite } from "../../src/validation/validators/validation-composite"
 import { ValidatorSpy } from "../presentation/controllers/mocks"
-
-class ValidationComposite implements Validator {
-  constructor(private readonly validators: Validator[]) { }
-  validate(input: any): Error {
-    for (const validator of this.validators) {
-      const error = validator.validate(input)
-      if (error) return error
-    }
-  }
-}
 
 const validators = [new ValidatorSpy(), new ValidatorSpy()]
 
