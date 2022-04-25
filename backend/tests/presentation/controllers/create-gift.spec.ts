06b0458e33fc7ef99f096017385f0b84fc3fffc9
KPMGE <jesuskevin254@gmail.com>
2022-04-25 18:36:51 -0300

    :white_check_mark: ensure CreateGiftController returns badRequest if validator returns error

--- backend/tests/presentation/controllers/create-gift.spec.ts
+++ backend/tests/presentation/controllers/create-gift.spec.ts
@@ -1,6 +1,8 @@
 import { Gift } from "../../../src/domain/entities";
 import { CreateGift } from "../../../src/domain/useCases";
-import { Controller, HttpResponse } from "../../../src/presentation/contracts"
+import { Controller, HttpResponse, Validator } from "../../../src/presentation/contracts"
+import { badRequest } from "../../../src/presentation/helpers";
+import { ValidatorSpy } from "./mocks";
 
 export namespace CreateGiftController {
   export type Request = {
@@ -27,9 +29,11 @@ const fakeRequest: CreateGiftController.Request = {
 }
 
 class CreateGiftController implements Controller {
-  constructor(private readonly createGiftService: CreateGift) { }
+  constructor(private readonly createGiftService: CreateGift, private readonly validator: Validator) { }
 
   async handle(request: CreateGiftController.Request): Promise<HttpResponse> {
+    const error = this.validator.validate(request)
+    if (error) return badRequest(error)
     this.createGiftService.execute(request)
     return null
   }
@@ -37,15 +41,19 @@ class CreateGiftController implements Controller {
 
 type SutTypes = {
   createGiftService: CreateGiftServiceSpy,
+  validatorSpy: ValidatorSpy,
   sut: CreateGiftController
 }
 
 const makeSut = (): SutTypes => {
   const createGiftService = new CreateGiftServiceSpy()
-  const sut = new CreateGiftController(createGiftService)
+  const validatorSpy = new ValidatorSpy()
+  validatorSpy.output = null
+  const sut = new CreateGiftController(createGiftService, validatorSpy)
   return {
     createGiftService,
-    sut
+    sut,
+    validatorSpy
   }
 }
 
@@ -54,5 +62,13 @@ describe('create-gift-controller', () => {
     const { sut, createGiftService } = makeSut()
     await sut.handle(fakeRequest)
     expect(createGiftService.input).toEqual(fakeRequest)
+  })
+
+  it('should return badRequest if validator returns error', async () => {
+    const { sut, validatorSpy } = makeSut()
+    validatorSpy.output = new Error('invalid field')
+    const httpResponse = await sut.handle(fakeRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('invalid field'))
   })
 })
