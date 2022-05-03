838d1def83b26f8de0d8b98e9ee490b947a623ea
KPMGE <jesuskevin254@gmail.com>
2022-05-03 13:43:03 -0300

    :white_check_mark: ensure DeleteGiftRepository returns right data on success

--- backend/tests/data/delete-gift.spec.ts
+++ backend/tests/data/delete-gift.spec.ts
@@ -5,16 +5,25 @@ import { DeleteGift } from "../../src/domain/useCases"
 class DeleteGiftService implements DeleteGift {
   constructor(private readonly deleteGiftRepo: DeleteGiftRepository) { }
   async execute(giftId: string): Promise<Gift> {
-    this.deleteGiftRepo.delete(giftId)
-    return null
+    const deletedGift = await this.deleteGiftRepo.delete(giftId)
+    return deletedGift
   }
+}
+
+const fakeGift: Gift = {
+  id: 'any_gift_id',
+  name: 'any_gift_name',
+  price: 200.5,
+  imageUrl: 'any_image_url',
+  description: 'any_description'
 }
 
 class DeleteGiftRepositoryMock implements DeleteGiftRepository {
   input: any
+  output = fakeGift
   async delete(giftId: string): Promise<Gift> {
     this.input = giftId
-    return null
+    return this.output
   }
 }
 
@@ -39,10 +48,17 @@ describe('delete-gift', () => {
     expect(deleteGiftRepo.input).toBe('any_gift_id')
   })
 
-  it('should throw if repository throws', () => {
+  it('should throw if repository throws', async () => {
     const { deleteGiftRepo, sut } = makeSut()
     deleteGiftRepo.delete = () => { throw new Error('repo error') }
     const promise = sut.execute('any_gift_id')
-    expect(promise).rejects.toThrowError(new Error('repo error'))
+    await expect(promise).rejects.toThrowError(new Error('repo error'))
+  })
+
+  it('should return the deleted gift from repository', async () => {
+    const { deleteGiftRepo, sut } = makeSut()
+    deleteGiftRepo.output = fakeGift
+    const deletedGift = await sut.execute('any_gift_id')
+    expect(deletedGift).toEqual(fakeGift)
   })
 })
