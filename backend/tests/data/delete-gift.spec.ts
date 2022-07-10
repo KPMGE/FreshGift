56d681969279663d124b3a1e013270fd14c726a0
KPMGE <jesuskevin254@gmail.com>
2022-07-10 18:35:51 -0300

    :white_check_mark: ensure DeleteGiftService returns GiftNotFoundError

--- backend/tests/data/delete-gift.spec.ts
+++ backend/tests/data/delete-gift.spec.ts
@@ -1,19 +1,11 @@
 import { DeleteGiftRepository, FindGiftRepository } from "../../src/data/contracts"
 import { Gift } from "../../src/domain/entities"
-import { DeleteGift, FindGift } from "../../src/domain/useCases"
-
-
-const fakeGift: Gift = {
-  id: 'any_gift_id',
-  name: 'any_gift_name',
-  price: 200.5,
-  imageUrl: 'any_image_url',
-  description: 'any_description'
-}
+import { DeleteGift } from "../../src/domain/useCases"
+import { makeFakeGift } from "../domain/mocks/gift"
 
 class FindGiftRepositoryMock implements FindGiftRepository {
   input: any
-  output = fakeGift
+  output = makeFakeGift()
   async find(giftId: string): Promise<Gift> {
     this.input = giftId
     return this.output
@@ -21,13 +13,21 @@ class FindGiftRepositoryMock implements FindGiftRepository {
 }
 
 class DeleteGiftRepositoryMock implements DeleteGiftRepository {
-  input: any
-  output = fakeGift
+  input = ""
+  output = makeFakeGift()
   async delete(giftId: string): Promise<Gift> {
     this.input = giftId
     return this.output
   }
 }
+
+class GiftNotFoundError extends Error {
+  constructor() {
+    super('gift not found!')
+    super.name = 'GiftNotFoundError'
+  }
+}
+
 class DeleteGiftService implements DeleteGift {
   constructor(
     private readonly deleteGiftRepo: DeleteGiftRepository,
@@ -35,7 +35,7 @@ class DeleteGiftService implements DeleteGift {
   ) { }
   async execute(giftId: string): Promise<Gift> {
     const foundGift = await this.findGiftRepo.find(giftId)
-    if (!foundGift) throw new Error('gift not found')
+    if (!foundGift) throw new GiftNotFoundError()
     const deletedGift = await this.deleteGiftRepo.delete(giftId)
     return deletedGift
   }
@@ -59,43 +59,55 @@ const makeSut = (): SutTypes => {
 }
 
 describe('delete-gift', () => {
-  it('should call repository with right gift id', () => {
+  it('should call repository with right gift id', async () => {
     const { deleteGiftRepo, sut } = makeSut()
-    sut.execute('any_gift_id')
-    expect(deleteGiftRepo.input).toBe('any_gift_id')
+
+    await sut.execute('any_gift_id')
+
+    expect(deleteGiftRepo.input).toEqual('any_gift_id')
   })
 
   it('should throw if repository throws', async () => {
     const { deleteGiftRepo, sut } = makeSut()
     deleteGiftRepo.delete = () => { throw new Error('repo error') }
+
     const promise = sut.execute('any_gift_id')
+
     await expect(promise).rejects.toThrowError(new Error('repo error'))
   })
 
   it('should return the deleted gift from repository', async () => {
     const { deleteGiftRepo, sut } = makeSut()
-    deleteGiftRepo.output = fakeGift
+    deleteGiftRepo.output = makeFakeGift()
+
     const deletedGift = await sut.execute('any_gift_id')
-    expect(deletedGift).toEqual(fakeGift)
+
+    expect(deletedGift).toEqual(makeFakeGift())
   })
 
   it('should call find repository with correct gift id', () => {
     const { findGiftRepo, sut } = makeSut()
+
     sut.execute('any_gift_id')
+
     expect(findGiftRepo.input).toBe('any_gift_id')
   })
 
   it('should throw if findGiftRepository throws', () => {
     const { findGiftRepo, sut } = makeSut()
     findGiftRepo.find = () => { throw new Error('repo error') }
+
     const promise = sut.execute('any_gift_id')
+
     expect(promise).rejects.toThrowError(new Error('repo error'))
   })
 
-  it('should throw error if no gift was found', () => {
+  it('should throw GiftNotFoundError if no gift was found', () => {
     const { findGiftRepo, sut } = makeSut()
     findGiftRepo.output = null
+
     const promise = sut.execute('any_gift_id')
-    expect(promise).rejects.toThrowError(new Error('gift not found'))
+
+    expect(promise).rejects.toThrowError(new GiftNotFoundError())
   })
 })
